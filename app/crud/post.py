27d378ec69cc06f8d1e16a807ef2ffe7f02b5f2a
TIm <tim.ohana@yandex.ru>
2025-10-16 19:21:25 +0300

    feat: add async in crud post

--- app/crud/post.py
+++ app/crud/post.py
@@ -1,4 +1,6 @@
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 from app.models.post import Post
 from app.schemas.post import PostCreate, PostUpdate
@@ -8,42 +10,55 @@ from app.utils import resolved_tags
 
 async def create_post(db: AsyncSession, post:PostCreate, user_id: int):
     new_post = Post(**post.model_dump(exclude={'tags'}), user_id=user_id)
-    new_post.tags = await resolved_tags(db, post.tags)
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
+    try:
+        new_post.tags = await resolved_tags(db, post.tags)
+        db.add(new_post)
+        await db.commit()
+    except IntegrityError as e:
+        await db.rollback()
+        raise e
+    await db.refresh(new_post)
     return new_post
 
 
-async def get_post_by_id(db: AsyncSession, post_id: int):
-    return db.query(Post).filter(Post.id == post_id).first()
+async def get_post_by_id(db: AsyncSession, post_id: int) -> Post | None:
+    post = await db.get(Post, post_id)
+    return post
 
 
-async def get_posts(db: AsyncSession, skip: int = 0, limit: int = 100):
-    return db.query(Post).offset(skip).limit(limit).all()
+async def get_posts_by_user(db: AsyncSession, user_id: int) -> list[Post]:
+    result = await db.execute(select(Post).where(Post.user_id == user_id))
+    posts = result.scalars().all()
+    return posts
 
 
-async def get_posts_by_category(db: AsyncSession, category: str):
-    return db.query(Post).filter(Post.category == category).all()
+async def get_posts_by_category(db: AsyncSession, category: str) -> list[Post]:
+    result = await db.execute(select(Post).where(Post.category == category))
+    posts = result.scalars().all()
+    return posts
 
 
-async def update_post(db: AsyncSession, post: Post, updates: PostUpdate):
+async def update_post(db: AsyncSession, post: Post, updates: PostUpdate) -> Post | None:
     if not post:
         return None
     for field, value in updates.model_dump(exclude={'tags'}, exclude_unset=True).items():
         setattr(post, field, value)
-    if updates.tags is not None:
-        new_tags = resolved_tags(db, updates.tags)
-        post.tags = new_tags
-    db.commit()
-    db.refresh(post)
+    try:
+        if updates.tags is not None:
+            new_tags = await resolved_tags(db, updates.tags)
+            post.tags = new_tags
+        await db.commit()
+    except IntegrityError as e:
+        await db.rollback()
+        raise e
+    await db.refresh(post)
     return post
 
 
-async def delete_post(db: AsyncSession, post_id: int):
-    post =  get_post_by_id(db, post_id)
+async def delete_post(db: AsyncSession, post_id: int) -> bool | None:
+    post = await db.get(Post, post_id)
     if not post:
         return None
-    db.delete(post)
-    db.commit()
+    await db.delete(post)
+    await db.commit()
     return True
